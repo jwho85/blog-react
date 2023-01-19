40e67503f5e9458e2c125c7aed2b98f9f45fc977
jwho85 <98196552+jwho85@users.noreply.github.com>
2023-01-18 18:00:30 -0800

    Removed alert from Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -25,7 +25,7 @@ export default function Dashboard() {
             setName(data.name);
         } catch (err) {
             console.error(err);
-            alert("An error occured while fetching user data");
+            // alert("An error occured while fetching user data");
         }
     };
 
