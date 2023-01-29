62d7da24faa9a88d6e52fb19694f9e42b80a2681
jwho85 <98196552+jwho85@users.noreply.github.com>
2023-01-28 16:56:22 -0800

    Added displayName update

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { auth, db, logout } from "./utils/firebase";
 import { collection, doc, addDoc, getDoc, getDocs, updateDoc, deleteDoc, Timestamp, query, orderBy, onSnapshot, where } from "firebase/firestore";
 import Menu from "./Menu";
+import { getAuth, updatePassword, updateProfile } from "firebase/auth";
 
 export default function Profile() {
 
@@ -12,8 +13,12 @@ export default function Profile() {
     const [user, loading, error] = useAuthState(auth);
     const [userInfo, setUserInfo] = useState([]);
     const navigate = useNavigate();
+    const [currentPassword, setCurrentPassword] = "";
+    const [newPassword, setNewPassword] = "";
 
     const { id } = useParams();
+
+    const currentUser = auth.currentUser;
 
     //function for automatically retrieving user
     useEffect(() => {
@@ -24,6 +29,7 @@ export default function Profile() {
                 data: doc.data()
             })))
         })
+        console.log(user.displayName);
     }, [])
 
     //function for updating a user
@@ -33,11 +39,23 @@ export default function Profile() {
             await updateDoc(userDocRef, {
                 name: name,
             })
-            alert("User updated successfully!")
+            updateProfile(auth.currentUser, {
+                displayName: name,
+            })
+            alert("User updated successfully!");
         } catch (err) {
             alert(err)
         }
     };
+
+    //function for updating password (not working)
+    const updateUserPassword = async (currentUser, newPassword) => {
+        updatePassword(currentUser, newPassword).then(() => {
+            alert("Password updated successfully!")
+        }).catch((err) => {
+            alert(err)
+        });
+    }
 
     return (
         <div>
@@ -60,8 +78,19 @@ export default function Profile() {
                         >
                             Update name
                         </button>
-                        <div className="users">
-                        </div>
+                        <input
+                            type="text"
+                            className="reset__textBox"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                            placeholder="Enter new password"
+                        />
+                        <button
+                            className="reset__btn"
+                            onClick={() => updateUserPassword(currentUser, newPassword)}
+                        >
+                            Update password
+                        </button>
                     </div>
                 </div>
             ))}
