6f90c0f0a0d686aae27c839d6e5b7b2a06ad7163
jwho85 <98196552+jwho85@users.noreply.github.com>
2023-02-14 21:22:29 -0800

    Added email and password verification

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,18 +3,27 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, useNavigate } from "react-router-dom";
 import { auth, registerWithEmailAndPassword, signInWithGoogle } from "./utils/firebase";
 import "./Register.css";
+import { verifyPasswordResetCode } from "firebase/auth";
 
 function Register() {
 
     const [email, setEmail] = useState("");
+    const [verifyEmail, setVerifyEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [verifyPassword, setVerifyPassword] = useState("");
     const [name, setName] = useState("");
     const [user, loading, error] = useAuthState(auth);
     const navigate = useNavigate();
 
     const register = () => {
         if (!name) alert("Please enter name");
-        registerWithEmailAndPassword(name, email, password);
+        if (email !== verifyEmail) {
+            alert("The email addresses do not match.");
+        } else if (password !== verifyPassword) {
+            alert("The passwords do not match.");
+        } else {
+            registerWithEmailAndPassword(name, email, password);
+        }
     };
 
     useEffect(() => {
@@ -42,11 +51,25 @@ function Register() {
                     placeholder="E-mail Address"
                 />
                 <input
+                    type="text"
+                    className="register__textBox"
+                    value={verifyEmail}
+                    onChange={(e) => setVerifyEmail(e.target.value)}
+                    placeholder="Verify E-mail Address"
+                />
+                <input
                     type="password"
                     className="register__textBox"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     placeholder="Password"
+                />
+                <input
+                    type="password"
+                    className="register__textBox"
+                    value={verifyPassword}
+                    onChange={(e) => setVerifyPassword(e.target.value)}
+                    placeholder="Verify Password"
                 />
                 <button className="register__btn" onClick={register}>
                     Register
